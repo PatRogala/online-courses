7067e35ff11743d2d6c60967a98b9771177926c2
Patryk Rogala <rogala.patryk@pm.me>
2022-11-20 19:54:45 +0100

    Racket basics

---
+++ Programming Languages: Part B/notes.rkt
@@ -0,0 +1,36 @@
+;; Installing
+;; First line
+#lang racket
+
+;; For Homeworks
+(provide (all-defined-out))
+
+;; For second file to test
+#lang racket
+(require "foo.rkt")
+
+;; Racket Definitions, Functions, Conditionals
+(define x 3)
+(define y (+ x 2))
+
+(define cube1
+  (lambda (x)
+    (* x (* x x))))
+
+(define cube2
+  (lambda (x)
+    (* x x x)))
+
+(define (cube3 x)
+  (* x x x))
+
+(define (pow1 x y)
+  (if (= y 0)
+      1
+      (* x (pow1 x (- y 1)))))
+
+;; Racket Lists
+(defina xs (list 1 2 3 4 5))
+(null? xs) ;; Check if list is empty
+(car xs) ;; First element
+(cdr xs) ;; Rest of list
