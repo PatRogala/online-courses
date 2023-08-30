05529faa481be7b6b4e17911179a867378b4d8c5
Patryk Rogala <rogala.patryk@pm.me>
2023-08-30 23:50:02 +0200

    Index Routes

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -14,6 +14,7 @@ import EditContact, {
   action as editAction,
 } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
