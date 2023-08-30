e328bcab93c68e7bfd2c84518683d59f6a6a354b
Patryk Rogala <rogala.patryk@pm.me>
2023-08-30 23:51:10 +0200

    Cancel Button

--- react-router-tutorial/src/routes/edit.jsx
+++ react-router-tutorial/src/routes/edit.jsx
@@ -2,6 +2,7 @@ import {
   Form,
   useLoaderData,
   redirect,
+  useNavigate,
 } from "react-router-dom";
 import { updateContact } from "../contacts";
 
@@ -14,6 +15,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -63,7 +65,14 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Cancel
+        </button>
       </p>
     </Form>
   );
