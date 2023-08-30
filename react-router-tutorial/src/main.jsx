f0150d66334e71548227fc5fb21d54106fc83bca
Patryk Rogala <rogala.patryk@pm.me>
2023-08-30 23:30:33 +0200

    Deleting Records

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -13,6 +13,7 @@ import Root, { loader as rootLoader, action as rootAction } from "./routes/root"
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
