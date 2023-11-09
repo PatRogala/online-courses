b603ac5da9d33768ca76739ab415b86e3eafb2ae
Patryk Rogala <patryk@astarium.pl>
2023-11-09 13:13:44 +0100

    Guessing Game

---
+++ app-academy-open/software-engineering-foundations/guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,31 @@
+class GuessingGame
+  attr_reader :num_attempts
+
+  def initialize(min, max)
+    @secret_num = rand(min..max)
+    @num_attempts = 0
+    @game_over = false
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(num)
+    @num_attempts += 1
+    @game_over = true if num == @secret_num
+    if num == @secret_num
+      puts 'you win'
+    elsif num > @secret_num
+      puts 'too big'
+    else
+      puts 'too small'
+    end
+  end
+
+  def ask_user
+    puts 'enter a number'
+    num = gets.chomp.to_i
+    check_num(num)
+  end
+end
