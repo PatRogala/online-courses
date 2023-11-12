367cb0ea1ee5d3ee46cc676e43fa585f7ae12840
Patryk Rogala <rogala.patryk@pm.me>
2023-11-12 19:55:32 +0100

    1.9: unicafe step4

--- fullstack-open/part1/unicafe/src/App.jsx
+++ fullstack-open/part1/unicafe/src/App.jsx
@@ -5,6 +5,15 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (all === 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <div>No feedback given</div>
+      </>
+    )
+  }
+
   return (
     <>
       <h2>statistics</h2>
