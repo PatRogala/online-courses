48d8dd0ecbfb9ca74230ce0233c7a0c8a14a7c77
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:22:27 +0100

    Don’t Conflate Helpers with Your Domain

--- sustainable-rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
@@ -12,6 +12,15 @@ class WidgetsController < ApplicationController
                               manufacturer_id: rand(100),
                               manufacturer: manufacturer,
                               name: "Widget #{params[:id]}")
+
+    def @widget.widget_id
+      if self.id.to_s.length < 3
+        self.id.to_s
+      else
+        self.id.to_s[0..-3] + "." +
+          self.id.to_s[-2..-1]
+      end
+    end
   end
 
   def index
