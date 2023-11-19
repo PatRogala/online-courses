e9b8659467be6f1e716cf34327689c3a1b3d09ad
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 14:00:43 +0100

    Test Against Default Markup and Content Initially

--- sustainable-rails/sustainable-rails-docker/widgets/test/system/view_widget_test.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/test/system/view_widget_test.rb
@@ -2,6 +2,20 @@ require "application_system_test_case"
 
 class ViewWidgetTest < ApplicationSystemTestCase
   test "we can see a list of widgets and view one" do
-    # test goes here
+    visit widgets_path
+
+    widget_name = "stembolt"
+    widget_name_regexp = /#{widget_name}/i
+
+    assert_selector "ul li", text: /flux capacitor/i
+    assert_selector "ul li", text: widget_name_regexp
+
+    find("ul li", text: widget_name_regexp).find("a").click
+
+    # remember, 1234 is formatted as 12.34
+    formatted_widget_id_regexp = /12\.34/
+
+    assert_selector "h1", text: widget_name_regexp
+    assert_selector "h2", text: formatted_widget_id_regexp
   end
 end
