b33f5ef5c604628dd1d1e54a9cb076e7575171f4
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 15:07:24 +0100

    Writing Tests for Database Constraints

--- sustainable-rails/sustainable-rails-docker/widgets/test/models/widget_test.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/test/models/widget_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class WidgetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    widget_status = WidgetStatus.create!(name: "fresh")
+    manufacturer = Manufacturer.create!(
+      name: "Cyberdyne Systems",
+      address: Address.create!(
+        street: "742 Evergreen Terrace",
+        zip: "90210"
+      )
+    )
+
+    @widget = Widget.create!(
+      name: "Stembolt",
+      manufacturer: manufacturer,
+      widget_status: widget_status,
+      price_cents: 10_00
+    )
+  end
+
+  test "valid prices do not trigger the DB constraint" do
+    assert_nothing_raised do
+      @widget.update_column(
+        :price_cents, 45_00
+      )
+    end
+  end
+
+  test "negative prices do trigger the DB constraint" do
+    ex = assert_raises do
+      @widget.update_column(
+        :price_cents, -45_00
+      )
+    end
+
+    assert_match(/price_must_be_positive/i,ex.message)
+  end
 end
