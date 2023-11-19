2283fbe94cfbc518990b55959117a07192b44b17
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:24:46 +0100

    Small, Inline Components

--- sustainable-rails/sustainable-rails-docker/widgets/app/helpers/application_helper.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     render partial: "widgets/rating",
            locals: { widget: widget, suppress_cta: suppress_cta }
   end
+
+  def styled_widget_id(formatted_id)
+    content_tag(:span, formatted_id, style: "font-family: monospace")
+  end
 end
