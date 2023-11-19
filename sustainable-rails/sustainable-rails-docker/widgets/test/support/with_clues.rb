bfad5bac4e33e39d9b034f69ed357e7b720c4de3
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 14:23:04 +0100

    Enhancing with_clues to Dump Browser Logs

--- sustainable-rails/sustainable-rails-docker/widgets/test/support/with_clues.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/test/support/with_clues.rb
@@ -7,6 +7,31 @@ module TestSupport
       block.()
     rescue Exception => ex
       puts "[ with_clues ] Test failed: #{ex.message}"
+      if page.driver.respond_to?(:browser)
+        if page.driver.browser.respond_to?(:manage)
+          if page.driver.browser.manage.respond_to?(:logs)
+            logs = page.driver.browser.manage.logs
+            browser_logs = logs.get(:browser)
+            browser_logs.each do |log|
+              puts log.message
+            end
+            puts "[ with_clues ] } END Browser Logs"
+          else
+            puts "[ with_clues ] NO BROWSER LOGS: " +
+                "page.driver.browser.manage " +
+                "#{page.driver.browser.manage.class} " +
+                "does not respond to #logs"
+          end
+        else
+          puts "[ with_clues ] NO BROWSER LOGS: " +
+              "page.driver.browser #{page.driver.browser.class} " +
+              "does not respond to #manage"
+        end
+      else
+        puts "[ with_clues ] NO BROWSER LOGS: page.driver " +
+            "#{page.driver.class} does not respond to #browser"
+      end
+      puts
       puts "[ with_clues ] HTML {"
       puts
       puts page.html
