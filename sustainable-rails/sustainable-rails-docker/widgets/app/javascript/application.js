e8c40708768942686fc8b5e0589554601a1e645e
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:45:00 +0100

    Tweak Turbo to Provide a Slightly Better Experience

--- sustainable-rails/sustainable-rails-docker/widgets/app/javascript/application.js
+++ sustainable-rails/sustainable-rails-docker/widgets/app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+
+// The default of 500ms is too long and
+// users can lose the casual link between clicking
+// a link and seeing the browser respons
+Turbo.setProgressBarDelay(100)
+
 import "controllers"
