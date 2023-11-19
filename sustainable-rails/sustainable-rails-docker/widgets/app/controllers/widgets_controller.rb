42cd1af524c439141fdb3822a39a552805bfd9f3
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:15:06 +0100

    Name the Instance Variable After the Resource

--- sustainable-rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/app/controllers/widgets_controller.rb
@@ -1,8 +1,17 @@
 class WidgetsController < ApplicationController
   def show
+    manufacturer = OpenStruct.new(
+      id: rand(100),
+      name: "Sector 7G",
+      address: OpenStruct.new(
+        id: rand(100),
+        country: "UK"
+      )
+    )
     @widget = OpenStruct.new(id: params[:id],
-                             manufacturer_id: rand(100),
-                             name: "Widget #{params[:id]}")
+                              manufacturer_id: rand(100),
+                              manufacturer: manufacturer,
+                              name: "Widget #{params[:id]}")
   end
 
   def index
