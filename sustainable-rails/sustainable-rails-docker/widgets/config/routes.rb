70ee0b5ad2eafb64c40e33e8833a13e13f77978f
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:04:42 +0100

    Namespacing Might (or Might Not) be an Architecture Smell

--- sustainable-rails/sustainable-rails-docker/widgets/config/routes.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  namespace :customer_service do
+    resources :widgets, only: [ :show, :update, :destroy ]
+  end
+
   ####
   # Custom routes start here
   #
