fbeb71a536a196a5a68cf3e50ae0bb93ddd5e188
Patryk Rogala <rogala.patryk@pm.me>
2023-11-19 13:23:36 +0100

    Wrapping Complex Partials

--- sustainable-rails/sustainable-rails-docker/widgets/app/helpers/application_helper.rb
+++ sustainable-rails/sustainable-rails-docker/widgets/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def widget_rating_component(widget, suppress_cta: false)
+    render partial: "widgets/rating",
+           locals: { widget: widget, suppress_cta: suppress_cta }
+  end
 end
